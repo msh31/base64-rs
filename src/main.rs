df6a438d48d2bf8363e3e6503de48dcf8a2f8424
Marco <180540729+msh31@users.noreply.github.com>
2025-11-03 21:04:38 +0100

    feat: add clap for cli parsinig
    so confusinig how this works... i triied my best with comments but its
    still not clear.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+// compilers writes this code
+// the derive for debubg, auto gens fmt::debug implementation for this struct
+// parser, same thing for passing in the arguments we want, defined in a struct
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)] //short = n, long = name, uses the variable name
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
 }
